5c47d0f8d1d737b1f2e9385c8263160561f8ca49
Robert Leggett <Robert.Leggett2@auspost.com.au>
2014-06-25 10:06:36 +1000

    Added angular service data tests Removed useless comments

--- src/test/webapp/WEB-INF/resources/js/custom/controller.spec.js
+++ src/test/webapp/WEB-INF/resources/js/custom/controller.spec.js
@@ -22,35 +22,30 @@ describe('CustomerController Tests', function() {
 
     describe('CustomerController Structural Tests', function() {
 
-        // check to see if it has the expected function
         it('should have an init function in scope', function () {
            expect(angular.isFunction($rootScope.init)).toBe(true);
         });
 
-        // check to see if it has the expected function
         it('should have an save function in scope', function () {
             expect(angular.isFunction($rootScope.save)).toBe(true);
         });
 
-        // check to see if it has the expected function
         it('should have an delete function in scope', function () {
             expect(angular.isFunction($rootScope.delete)).toBe(true);
         });
     });
 
-    describe('CustomerController Customers Tests', function() {
+    describe('CustomerController Init Tests', function() {
 
         beforeEach(function() {
             //setup the spy to always return the spyPromise for the entire test spec
             mockCustomerService.getCustomers.and.returnValue(spyPromise);
         });
 
-        // check that the customers starts undefined
         it('should initially has customers undefined before any calls are made', function(){
             expect($rootScope.customers).toEqual(undefined);
         });
 
-        // check that scope variable is populated
         it('should have scope variable customers populated', function () {
             $rootScope.init();
 
@@ -86,7 +81,6 @@ describe('MainController Tests', function() {
         });
     });
 
-    // check to see if it has the expected function
     it('should have an logout function in scope', function () {
         expect(angular.isFunction($rootScope.logout)).toBe(true);
     });
@@ -109,7 +103,6 @@ describe('LoginController Tests', function() {
         });
     });
 
-    // check to see if it has the expected function
     it('should have an login function in scope', function () {
         expect(angular.isFunction($rootScope.login)).toBe(true);
     });
--- src/test/webapp/WEB-INF/resources/js/custom/service.spec.js
+++ src/test/webapp/WEB-INF/resources/js/custom/service.spec.js
@@ -1,5 +1,5 @@
 describe('CustomerService Tests', function (){
-    var CustomerService;
+    var $httpBackend, CustomerService;
 
     // excuted before each "it" is run.
     beforeEach(function (){
@@ -11,24 +11,53 @@ describe('CustomerService Tests', function (){
         // The _underscores_ are a convenience thing
         // so you can have your variable name be the
         // same as your injected service.
-        inject(function(_CustomerService_) {
+        inject(function(_$httpBackend_, _CustomerService_) {
+            $httpBackend = _$httpBackend_;
             CustomerService = _CustomerService_;
         });
     });
 
-    // check to see if it has the expected function
-    it('should have an get customers function', function () {
-        expect(angular.isFunction(CustomerService.getCustomers)).toBe(true);
+    describe('CustomerService Structural Tests', function() {
+
+        it('should have an get customers function', function () {
+            expect(angular.isFunction(CustomerService.getCustomers)).toBe(true);
+        });
+
+        it('should have an delete customer function', function () {
+            expect(angular.isFunction(CustomerService.deleteCustomer)).toBe(true);
+        });
+
+        it('should have an save customer function', function () {
+            expect(angular.isFunction(CustomerService.saveCustomer)).toBe(true);
+        });
     });
 
-    // check to see if it has the expected function
-    it('should have an delete customer function', function () {
-        expect(angular.isFunction(CustomerService.deleteCustomer)).toBe(true);
-    });
+    describe('CustomerService Get Customers Tests', function() {
 
-    // check to see if it has the expected function
-    it('should have an save customer function', function () {
-        expect(angular.isFunction(CustomerService.saveCustomer)).toBe(true);
+        var populatedData;
+
+        beforeEach(function () {
+            populatedData = [
+                {"id": 1, "firstName": "Foo", "lastName": "Bar"},
+                {"id": 2, "firstName": "Jim", "lastName": "Sunny"},
+                {"id": 3, "firstName": "Peter", "lastName": "Prone"},
+                {"id": 4, "firstName": "Sam", "lastName": "Sully"}
+            ];
+        });
+
+        it('should return results from get customers function call', function () {
+            $httpBackend.whenGET('customer/customers/retrieve').respond(populatedData);
+
+            // check result returned from service call
+            var promise = CustomerService.getCustomers();
+            promise.then(function (data) {
+                expect(data).toEqual(populatedData);
+            });
+
+            $httpBackend.flush();
+
+            $httpBackend.expectGET('customer/customers/retrieve').respond(populatedData);
+        });
     });
 });
 
@@ -50,7 +79,6 @@ describe('AuthenticationService Tests', function (){
         });
     });
 
-    // check to see if it has the expected function
     it('should have a logout function', function () {
         expect(angular.isFunction(AuthenticationService.logout)).toBe(true);
     });
@@ -74,12 +102,10 @@ describe('Base64Service Tests', function (){
         });
     });
 
-    // check to see if it has the expected function
     it('should have an encode function', function () {
         expect(angular.isFunction(Base64Service.encode)).toBe(true);
     });
 
-    // check to see if it has the expected function
     it('should have an decode function', function () {
         expect(angular.isFunction(Base64Service.decode)).toBe(true);
     });
